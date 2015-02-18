b9be450b148baf50666f5bc527274297c9ce11f5
Duncan Roze <duncanroze@MacBook-Pro-de-Duncan.local>
2015-02-18 10:13:03 +0100

    Version 1.1

--- pimodule.php
+++ pimodule.php
@@ -1,54 +1,123 @@
 <?php
-
 if(!defined('_PS_VERSION_'))
-    exit;
+	exit;
 
-class PiModule extends Module{
+class PiModule extends Module
+{
+	public function __construct()
+	{
+		$this->name = 'pimodule';
+		$this->tab = 'Administration';
+		$this->version = '1.0.0';
+		$this->author = 'PimoTeamolo';
+		$this->need_instance = 1;
+		$this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
+		$this->bootstrap = true;
 
+		parent::__construct();
 
-    public function __construct(){
+		$this->displayName = $this->l('Pimodule');
+		$this->description = $this->l('C\'est le module du Swaggymolo.');
 
+		$this->confirmUninstall = $this->l('Voulez vous vraiment le désinstaller ?');
 
-        $this->name = 'pimodule';
-        $this->tab = 'Administration';
-        $this->version= '1.0.0';
-        $this->author='PimoTeamolo';
-        $this->need_instance='1';
-        $this->ps_version_compilancy = array('min' => '1.6', 'max' => _PS_VERSION_);
-        $this->bootstrap = true;
+		if (!Configuration::get('PIMODULE_NAME'))
+			$this->warning = $this->l('No name provided');
+	}
 
-        parent::__construct();
+	public function install()
+	{
+		if (!parent::install() ||
+			!Configuration::updateValue('PIMODULE_NAME','my friend'))
+			return false;
+		return true;
+	}
 
-        $this->displayName = $this->l('Pimodule');
-        $this->description = $this->l('C\'est le module du Swaggymolo.');
+	public function uninstall()
+	{
+		if (!parent::uninstall())
+			return false;
+		return true;
+	}
 
-        $this->confirmUninstall = $this->l('Are you sure you want to uninstall?');
+	public function getContent()
+	{
+		$output = null;
 
+		if (Tools::isSubmit('submit'.$this->name))
+		{
+			$pimodule = strval(Tools::getValue('PIMODULE_NAME'));
+			if (!$pimodule
+			  || empty($pimodule)
+			  || !Validate::isGenericName($pimodule))
+				$output .= $this->displayError($this->l('Invalid Configuration value'));
+			else
+			{
+				Configuration::updateValue('PIMODULE_NAME', $pimodule);
+				$output .= $this->displayConfirmation($this->l('Settings updated'));
+			}
+		}
+		return $output.$this->displayForm();
+	}
 
-        if (!Configuration::get('PIMODULE_NAME')){
-            $this->warning = $this->l('No name provided');
-        }
+	public function displayForm()
+	{
+		// Get default language
+		$default_lang = (int)Configuration::get('PS_LANG_DEFAULT');
 
-    }
+		// Init Fields form array
+		$fields_form[0]['form'] = array(
+			'legend' => array(
+				'title' => $this->l('Settings'),
+			),
+			'input' => array(
+				array(
+					'type' => 'text',
+					'label' => $this->l('Configuration value'),
+					'name' => 'MYMODULE_NAME',
+					'size' => 20,
+					'required' => true
+				)
+			),
+			'submit' => array(
+				'title' => $this->l('Save'),
+				'class' => 'button'
+			)
+		);
 
-    public function install(){
+		$helper = new HelperForm();
 
-        if(!parent::install() || !Configuration::updateValue('MYMODULE_NAME','Pimodule')){
-               return false;
-        }
+		// Module, token and currentIndex
+		$helper->module = $this;
+		$helper->name_controller = $this->name;
+		$helper->token = Tools::getAdminTokenLite('AdminModules');
+		$helper->currentIndex = AdminController::$currentIndex.'&configure='.$this->name;
 
-        return true;
-    }
+		// Language
+		$helper->default_form_language = $default_lang;
+		$helper->allow_employee_form_lang = $default_lang;
 
+		// Title and toolbar
+		$helper->title = $this->displayName;
+		$helper->show_toolbar = true; // false -> remove toolbar
+		$helper->toolbar_scroll = true; // yes - > Toolbar is always visible on the top of the screen.
+		$helper->submit_action = 'submit'.$this->name;
+		$helper->toolbar_btn = array(
+			'save' =>
+			array(
+				'desc' => $this->l('Save'),
+				'href' => AdminController::$currentIndex.'&configure='.$this->name.'&save'.$this->name.
+				'&token='.Tools::getAdminTokenLite('AdminModules'),
+			),
+			'back' => array(
+				'href' => AdminController::$currentIndex.'&token='.Tools::getAdminTokenLite('AdminModules'),
+				'desc' => $this->l('Back to list')
+			)
+		);
 
-    public function uninstall(){
-        
-        if(!parent::uninstall()){
-            return false;
-        }
+		// Load current value
+		$helper->fields_value['MYMODULE_NAME'] = Configuration::get('MYMODULE_NAME');
 
-    return true;
-
-    }
-
+		return $helper->generateForm($fields_form);
+	}
 }
